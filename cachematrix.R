8ef3a9d0ad497df11a1fa7f1fbf6567e27dcedb7
Nicholas Ricciarelli <nar5501@rit.edu>
2022-08-10 16:39:02 -0400

    finish matrix code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Nick Ricciarelli
+## R Programming
+## Programming Assingment 2
 
-## Write a short comment describing this function
-
+## Create matrix obj and cache it's inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+        inverse <- NULL
+        set <- function(y) {
+          x <<- y
+          inverse <<- NULL
+        }
+        get <- function() {
+          x
+        }
+        setInverse <- function(i) {
+          inverse <<- i
+        }
+        getInverse <- function () {
+          inverse
+        }
+    
+        list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## Compute the inverse of the matrix if not already cached, if cached return
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inverse <- x$getInverse()
+
+        if(!is.null(inverse)) {
+          return(inverse)
+        }
+
+        matrix <- x$get()
+        inverse <- solve(matrix, ...)
+        x$setInverse(inverse)
+        inverse
 }
+
